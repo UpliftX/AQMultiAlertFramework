d173840ab20babf654109b38303ddb3c5ad292ec
Mrigank Gupta <mrigank.gupta@honeywell.com>
2016-06-16 15:15:21 +0530

    Initial Commit

---
+++ AQMultiAlertFramework/AQMultiAlertFramework.h
@@ -0,0 +1,19 @@
+//
+//  AQMultiAlertFramework.h
+//  AQMultiAlertFramework
+//
+//  Created by Gupta, Mrigank on 16/06/16.
+//  Copyright © 2016 Gupta, Mrigank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AQMultiAlertFramework.
+FOUNDATION_EXPORT double AQMultiAlertFrameworkVersionNumber;
+
+//! Project version string for AQMultiAlertFramework.
+FOUNDATION_EXPORT const unsigned char AQMultiAlertFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AQMultiAlertFramework/PublicHeader.h>
+
+
